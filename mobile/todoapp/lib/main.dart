6154ce47a0e799d1ee7b7964904be3e79af43944
e7gx <abalgabo@gmail.com>
2024-08-02 23:50:52 +0300

    added the dark mode

--- mobile/todoapp/lib/main.dart
+++ mobile/todoapp/lib/main.dart
@@ -12,6 +12,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Todo App',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+        fontFamily: 'Cairo', // Apply the custom font family here
+        appBarTheme: AppBarTheme(
+          color: Colors.blueGrey[900], // Custom color for AppBar
+        ),
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(color: Colors.white),
+          bodyMedium: TextStyle(color: Colors.white70),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.blueGrey[700], // Text color
+          ),
+        ),
+      ),
       home: HomeScreen(),
     );
   }
