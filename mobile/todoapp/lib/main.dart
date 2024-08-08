886c68a70506463d6d7fec08330d1fde12a0bdd9
e7gx <abalgabo@gmail.com>
2024-08-08 22:50:43 +0300

    added shared_preferences_service to get the username for messages and appbar etc...

--- mobile/todoapp/lib/main.dart
+++ mobile/todoapp/lib/main.dart
@@ -16,6 +16,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.light,
         scaffoldBackgroundColor: Colors.white,
+        primaryColor: Colors.black,
+        splashColor: Color(0x69494444),
         cardColor: Colors.white,
         canvasColor: Colors.white,
         fontFamily: 'Cairo', // Apply the custom font family here
@@ -29,10 +31,25 @@ class MyApp extends StatelessWidget {
             borderRadius: BorderRadius.circular(8.0),
           ),
         ),
+        checkboxTheme: CheckboxThemeData(
+          checkColor: WidgetStateProperty.resolveWith((states) => Colors.green),
+          fillColor: WidgetStateProperty.resolveWith((states) => Colors.white),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(1.0),
+          ),
+          overlayColor: WidgetStateProperty.resolveWith((states) => Colors.red),
+        ),
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 80,
+              vertical: 15,
+            ),
             foregroundColor: Colors.white,
             backgroundColor: Colors.black, // Text color
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
           ),
         ),
       ),
